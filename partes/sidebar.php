93a4eace14f3976b89af413407e829c6d65e4103
Jorge Aníbal Fernández Enríquez <jorge.fernandez1801@alumnos.ubiobio.cl>
2022-04-16 17:52:33 -0400

    Initial commit

---
+++ partes/sidebar.php
@@ -0,0 +1,39 @@
+<!-- Sidebar -->
+<div id="sidebar-container" class="bg-primary">
+            <div class="logo">
+                <h4><a class="text-light font-weight-bold mb-0" href="../" class="d-block text-light p-3 border-0">
+                INGENIERÍA DE SOFTWARE</a></h4>
+            </div>
+            <div class="menu">
+                <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-tachometer-alt"></i>
+                    Inicio</a>
+
+                <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-users"></i>
+                    Usuarios</a>
+
+                <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-drumstick-bite"></i>
+                    Gastos comunes</a>
+
+                <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-drumstick-bite"></i>
+                Comunicación</a>
+
+                <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-drumstick-bite"></i>
+                Reserva de espacios comunes </a>
+
+                <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-drumstick-bite"></i>
+                Visitas</a>
+
+                <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-drumstick-bite"></i>
+                Mantencion de instalaciones</a>
+
+                <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-chart-line"></i>
+                    Estadísticas</a>
+
+                <a href="../perfil/" class="d-block text-light p-3 border-0"><i class="fas fa-user"></i>
+                    Perfil</a>
+
+                <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-sliders-h"></i>
+                    Configuración</a>
+            </div>
+        </div>
+        <!-- Fin sidebar -->
