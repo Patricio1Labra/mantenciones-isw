d95524e74d8a74f66dab08de75d15b5a74f69805
Patricio Andrés Labra Medina <patricio.labra1801@alumnos.ubiobio.cl>
2022-05-10 17:53:55 -0400

    Cambiada base de datos

--- con_db.php
+++ con_db.php
@@ -1,8 +1,8 @@
 <?php
-    $server = "localhost";
-    $user = "root";
-    $contra = "";
-    $bd = "test";
+    $server = "146.83.194.142";
+    $user = "E5software";
+    $contra = "E5software1122";
+    $bd = "E5software_bd";
     $conex =new mysqli($server,$user,$contra,$bd);
         if(!$conex){
             echo "error en el servidor";
