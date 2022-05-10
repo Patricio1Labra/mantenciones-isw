f33fb814a20cc0e79a562b4d2c723591ca56dbce
Jorge Aníbal Fernández Enríquez <jorge.fernandez1801@alumnos.ubiobio.cl>
2022-05-10 03:26:28 -0400

    registrar mantencion por juan ito fecha y duracion

--- HU3/index.php
+++ HU3/index.php
@@ -19,37 +19,47 @@
         <!-- Page Content -->
         <div id="content" class="bg-grey w-100">
         <?php include('../con_db.php') ?>
-        <div class="col-lg-6 my-3">
-        <div class="card rounded-0">
-            <div class="card-header bg-light">
-                <h6 class="font-weight-bold mb-0">Registrar mantencion</h6>
+        <div class="container m-2">
+            <div class="row">
+                <div class="col-12 my-3">
+                    <div class="card rounded-0">
+                        <div class="card-header bg-light">
+                            <h6 class="font-weight-bold mb-0">Registrar mantencion</h6>
+                        </div>
+                        <div class="card-body pt-2">
+                        <form  method="post" action="">
+                        <label class="form-label" for="Nombre">Ingresar Título</label>    
+                        <input class="form-control" type="text" name="Nombre" placeholder="Nombre">
+                        <br>
+                        <label class="form-label" for="Tipo">Ingresar Tipo</label>
+                        <select class="form-control" name="Tipo" id="">
+                        <option value="0">Seleccionar Tipo</option>
+                        <?php
+                            $sqlc = "SELECT `IDT` as id , `TIPOTITULO` as nombre FROM `TIPO`";
+                            $resultadoc= mysqli_query($conex,$sqlc);
+                            while($valores = mysqli_fetch_array($resultadoc)) {
+                            echo '<option value="'. $valores['id'].'">'. $valores['nombre'].'</option>';
+                            }
+                        ?>
+                        </select>
+                        <br>
+                        <label class="form-label" for="Tipo">Ingresar Descripcion</label>
+                        <input class="form-control" type="text" name="Descripcion" placeholder="Descripcion">
+                        <br>
+                        <label class="form-label" for="Fecha">Ingresar Fecha</label>
+                        <input class="form-control" type="date" name="Fecha" placeholder="Fecha">
+                        <br>
+                        <label class="form-label" for="Duracion">Ingresar Duración</label>
+                        <input class="form-control" type="text" name="Duracion" placeholder="Duracion">
+                        <br>
+                        <input class="btn btn-primary btn-sm" type="submit" name="Siguiente">
+                        <?php
+                            include("../HU3/registro.php");
+                        ?>
+                        </form>
+                    </div>
+                </div>    
             </div>
-            <div class="card-body pt-2">
-            <form  method="post" action="">
-            <label class="form-label" for="Nombre">Ingresar Título</label>    
-            <input class="form-control" type="text" name="Nombre" placeholder="Nombre">
-            <br>
-            <label class="form-label" for="Tipo">Ingresar Tipo</label>
-            <select class="form-control" name="Tipo" id="">
-            <option value="0">Seleccionar Tipo</option>
-            <?php
-                $sqlc = "SELECT `IDT` as id , `TIPOTITULO` as nombre FROM `TIPO`";
-                $resultadoc= mysqli_query($conex,$sqlc);
-                while($valores = mysqli_fetch_array($resultadoc)) {
-                echo '<option value="'. $valores['id'].'">'. $valores['nombre'].'</option>';
-                }
-            ?>
-            </select>
-            <br>
-            <label class="form-label" for="Tipo">Ingresar Descripcion</label>
-            <input class="form-control" type="text" name="Descripcion" placeholder="Descripcion">
-            <br>
-            <input class="btn btn-primary btn-sm" type="submit" name="Siguiente">
-            <?php
-                include("../HU3/registro.php");
-            ?>
-        </form>
-        </div>
         </div>
 
                 
--- HU3/registro.php
+++ HU3/registro.php
@@ -6,11 +6,39 @@ if(isset($_POST['Siguiente'])){
     $nombre = $_POST['Nombre'];
     $tipo = $_POST['Tipo'];
     $descripcion = $_POST['Descripcion'];
-    if(!empty($nombre) && !empty($tipo) && !empty($descripcion)){
+    $fecha = $_POST['Fecha'];
+    $duracion = $_POST['Duracion'];
+    $ID;
+    $IDM;
+    if(!empty($nombre) && $tipo!=0 && !empty($descripcion)){
         $consulta = "INSERT INTO MANTENCION VALUES ('','$tipo','$nombre','$descripcion','P','')";
         $resultado = mysqli_query($conex,$consulta);
         if($resultado) {
-            
+            if(!empty($fecha) && !empty($duracion)){
+                $consultaid ="SELECT IDE as id FROM ENCARGADO  WHERE NOMBRE ='Juan ito'";
+                $res = mysqli_query($conex,$consultaid);
+                if($res) {
+                    while($valores = $res->fetch_array()) {
+                        $ID= $valores['id'];
+                    }
+                }
+                $consultaidm ="SELECT IDM as id FROM MANTENCION  WHERE IDT ='$tipo' && TITULO ='$nombre' && DESCRIPCION ='$descripcion'";
+                $res = mysqli_query($conex,$consultaidm);
+                if($res) {
+                    while($valoresm = $res->fetch_array()) {
+                        $IDM= $valoresm['id'];
+                    }
+                }
+                $consulta = "INSERT INTO ENCARGA VALUES ('$ID','$IDM','$fecha','$duracion')";
+                $resultado = mysqli_query($conex,$consulta);
+                if($resultado) {
+                    
+                    
+                }else{
+                    echo "ha ocurrido un error";
+                }
+        
+            }
             
         }else{
             echo "ha ocurrido un error";
