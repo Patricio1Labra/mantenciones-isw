f1f3adadf52be5c0d76b6bd45f6aebbb68d7cce8
Luis Vasquez Fica <luis.vasquez1801@alumnos.ubiobio.cl>
2022-05-24 20:18:41 -0400

    cambiar bdd

--- con_db.php
+++ con_db.php
@@ -1,11 +1,10 @@
 <?php
- $server = "localhost";
- $user = "root";
- $contra = "";
- $bd = "E5software_bd";
- $conex =new mysqli($server,$user,$contra,$bd);
-     if(!$conex){
-         echo "error en el servidor";
-     }
-
+    $server = "146.83.194.142";
+    $user = "E5software";
+    $contra = "E5software1122";
+    $bd = "E5software_bd";
+    $conex =new mysqli($server,$user,$contra,$bd);
+        if(!$conex){
+            echo "error en el servidor";
+        }
 ?>
