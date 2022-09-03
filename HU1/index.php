b6fb41f60e46323783ed88b90478b9bcb4baf321
Patricio Andrés Labra Medina <patricio.labra1801@alumnos.ubiobio.cl>
2022-09-03 12:55:05 -0400

    script confirmar funcionando en js

--- HU1/index.php
+++ HU1/index.php
@@ -96,29 +96,8 @@
                 border-top: 4px solid white;
             }
         </style>
-        <script>
-            function confirmar(id){
-                Swal.fire({
-                title: 'Estas seguro',
-                text: "No puedes recuperar mantencion",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Si, borremosla',
-                cancelButtonText: 'Cancelar'
-                }).then((result) => {
-                if (result.isConfirmed) {
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'La peticion ha sido borrada',
-                        showConfirmButton: false
-                    })
-                    window.location = './eliminar.php?idm='+id
-                }
-                })
-            }
-        </script>
+        <script src="../scripts/confirmar.js"></script>
+        
 
 </body>
 
