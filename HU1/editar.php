f73f41dd7583155f046ef847bc158adfba634d76
Patricio Andrés Labra Medina <patricio.labra1801@alumnos.ubiobio.cl>
2022-09-03 12:59:50 -0400

    script enviar en editar funcionando en js

--- HU1/editar.php
+++ HU1/editar.php
@@ -141,27 +141,7 @@
     <script src="../scripts/validanumeroyletra.js"></script>
     <script src="../scripts/contador.js"></script>
     <script src="../scripts/regresar.js"></script>
-    
-
-    <script>
-        function enviar(){
-            Swal.fire({
-            title: 'Estas seguro',
-            text: "Revise sus datos antes de enviar",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Si, enviar',
-            cancelButtonText: 'Cancelar'
-            }).then((result) => {
-            if (result.isConfirmed) {
-                document.getElementById("Enviar").value="a";
-                document.getElementById("formul").submit();
-            }
-            })
-        }
-    </script>
+    <script src="../scripts/enviar.js"></script>
 
 </body>
 
